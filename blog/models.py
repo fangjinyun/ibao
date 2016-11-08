1f7d4f65eb0ffe62503cfb46b67cbb536d54b55e
fangjinyun <elliotbao@outlook.com>
2016-11-08 21:53:18 +0800

    models,admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    #author = models.ForeignKey('auth.User')
+    title = models.CharField('标题', max_length=200)
+    text = models.TextField('正文')
+    created_date = models.DateTimeField('创建时间', auto_now_add=True)
+    published_date = models.DateTimeField('发布日期', blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'ibao',
+         'USER': 'ibao',
+         'PASSWORD': 'm',
+         'HOST': '127.0.0.1',
+         'PORT': '5432',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
