8a18acb103d31fd6c9422e241ab7aad1704d5761
fangjinyun <elliotbao@outlook.com>
2016-11-09 22:12:28 +0800

    draft

--- blog/views.py
+++ blog/views.py
@@ -11,13 +11,17 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()# remove this line,new posts will be saved as drafts that we can review later on rather than being instantly published
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -31,7 +35,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
